0d64804a462b7633183748f648b9ce7472b23a57
florianthiery <mail@fthiery.de>
2023-07-13 10:19:37 +0000

    Deploying to gh-pages from @ Research-Squirrel-Engineers/AtlantGIS_LOD@c59e830e506e9ae76dfef42251e54dd5ce8d72d7 🚀

--- atlantgis_classtree.js
+++ atlantgis_classtree.js
@@ -163,16 +163,16 @@ var tree={
       },
       {
         "id": "http://atlantgis.squirrel.link/ontology/CoastLine",
-        "parent": "Nfc1fe806472448f5a4cb20c54a556819",
+        "parent": "N4391eda768164ee4a4bef057412761a7",
         "type": "class",
         "text": "CoastLine (atlantgis:CoastLine)",
         "data": {}
       },
       {
-        "id": "Nfc1fe806472448f5a4cb20c54a556819",
+        "id": "N4391eda768164ee4a4bef057412761a7",
         "parent": "#",
         "type": "class",
-        "text": "Nfc1fe806472448f5a4cb20c54a556819",
+        "text": "N4391eda768164ee4a4bef057412761a7",
         "data": {}
       },
       {
@@ -3347,13 +3347,13 @@ var tree={
               "instancecount": 1
             },
             "http://www.w3.org/2000/01/rdf-schema#member": {
-              "instancecount": 122,
-              "http://atlantgis.squirrel.link/ontology/ArchaeologicalSite": 122
+              "instancecount": 10,
+              "http://atlantgis.squirrel.link/ontology/Site": 10
             }
           },
           "from": {}
         },
-        "instancecount": 125
+        "instancecount": 13
       },
       {
         "id": "http://atlantgis.squirrel.link/data/Find_collection",
@@ -3390,13 +3390,13 @@ var tree={
               "instancecount": 1
             },
             "http://www.w3.org/2000/01/rdf-schema#member": {
-              "instancecount": 5,
-              "http://atlantgis.squirrel.link/ontology/Period": 5
+              "instancecount": 136,
+              "http://atlantgis.squirrel.link/ontology/Find": 136
             }
           },
           "from": {}
         },
-        "instancecount": 7
+        "instancecount": 138
       }
     ]
   }
