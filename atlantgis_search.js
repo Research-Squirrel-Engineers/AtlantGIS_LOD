130c6baf8cc628fda8cf331d57d8c94008cd20c8
florianthiery <mail@fthiery.de>
2023-07-13 09:45:35 +0000

    Deploying to gh-pages from @ Research-Squirrel-Engineers/AtlantGIS_LOD@0e7b2297239ce3ef26d295aeefcf92d50244c5f6 🚀

--- atlantgis_search.js
+++ atlantgis_search.js
@@ -1,9 +1,4 @@
 var search={
-  "ArchaeologicalSite Instances Collection": "http://atlantgis.squirrel.link/data/ArchaeologicalSite_collection",
-  "Find Instances Collection": "http://atlantgis.squirrel.link/data/Find_collection",
-  "MultiPoint Instances Collection": "http://atlantgis.squirrel.link/data/MultiPoint_collection",
-  "Period Instances Collection": "http://atlantgis.squirrel.link/data/Period_collection",
-  "Point Instances Collection": "http://atlantgis.squirrel.link/data/Point_collection",
-  "Site Instances Collection": "http://atlantgis.squirrel.link/data/Site_collection",
-  "VesselType Instances Collection": "http://atlantgis.squirrel.link/data/VesselType_collection"
+  "Point Instances Collection": "http://atlantgis.squirrel.link/data/site/Point_collection",
+  "Site Instances Collection": "http://atlantgis.squirrel.link/data/site/Site_collection"
 }
